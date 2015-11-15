9d935f2a4614874fb935b2b669cfc306bad728b6
lzrp <prezlator@gmail.com>
2015-11-15 15:14:35 +0100

    AiTests implemented

--- tictactoe/tictactoeTests/Classes/AiTests.cs
+++ tictactoe/tictactoeTests/Classes/AiTests.cs
@@ -1,26 +1,52 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tictactoe.Classes;
+using tictactoeTests.Classes;
 
 namespace tictactoeTests.Classes
 {
     [TestClass()]
     public class AiTests
     {
+        readonly string[,] _board = { { " ", "", "X" }, { "O", " ", "X" }, { " ", " ", "" } };
+
         [TestMethod()]
-        public void AiTest()
+        public void Ai_BoardAssignmentOnConstructorCallTest()
         {
-            Assert.Fail();
+            // Assign
+            // Act
+            Ai computerPlayer = new Ai(_board);
+            
+            // Assert
+            Assert.AreEqual(computerPlayer.Board, _board);
         }
 
         [TestMethod()]
-        public void IsBoardFieldEmptyTest()
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void IsBoardFieldEmpty_BoardCoordinatesOutOfRangeTest()
         {
-            Assert.Fail();
+            // Assign
+            string[,] board = _board;
+            
+            // Act
+            Ai.IsBoardFieldEmpty(board, 4, 5);
+
+            // Assert
+            Assert.Fail("No exception was thrown.");
         }
 
         [TestMethod()]
-        public void PerformMoveTest()
+        public void PerformMove_EasyAiDifficultyTest()
         {
-            Assert.Fail();
+            // Assign
+            Ai computerPlayer = new Ai(_board);
+            Ai.Move expectedMove = new Ai.Move() {X = 2, Y = 0};
+
+            // Act
+            Ai.Move computersMove = computerPlayer.PerformMove("X");
+            
+            // Assert
+            Assert.AreEqual(expectedMove, computersMove);
         }
     }
 }
